d822aefcf838cfd5f8ac85912522be401b8149e5
Samuel <bdzsamuel@gmail.com>
2021-05-21 10:52:23 +0200

    Añadida funcionalidad de insertarDatos y system.properties

--- src/main/java/es/cebem/aireatetu/controllers/APIController.java
+++ src/main/java/es/cebem/aireatetu/controllers/APIController.java
@@ -1,17 +1,28 @@
 package es.cebem.aireatetu.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import es.cebem.aireatetu.models.DatosModel;
+import es.cebem.aireatetu.services.DatosService;
 
 @RestController
 public class APIController {
-  
-  // TODO: Añadir datos Service!
+
+  @Autowired
+  DatosService datosService;
 
   @GetMapping("/api/insertarDatos")
-  public String insertarDatos(@RequestParam String temp, @RequestParam String humd, @RequestParam String co2) {
-    return temp + ", " + humd + ", " + co2;
+  public String insertarDatos(@RequestParam String temp, @RequestParam String humd,
+      @RequestParam String co2) {
+    DatosModel datosModel = new DatosModel();
+    datosModel.setTemperature(Float.parseFloat(temp));
+    datosModel.setCo2(Float.parseFloat(co2));
+    datosModel.setHumidity(Float.parseFloat(humd));
+
+    datosService.addDatos(datosModel);
+    return temp + ", " + humd + ", " + co2 + ": SUCCESS!";
   }
 
 }
